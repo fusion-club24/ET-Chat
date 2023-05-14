fc912ca725731b5c62684007b2fc5f2cf71a44a7
21Matze <Matthias.schubert_1987@web.de>
2023-05-14 21:40:21 +0200

    Update ReloaderMessages.class.php

--- class/ReloaderMessages.class.php
+++ class/ReloaderMessages.class.php
@@ -328,12 +328,12 @@ class ReloaderMessages extends DbConectionMaker
 	* @return void
 	*/
 	private function blockiere($user_id, $privat_id){
-	#	if (isset ($_SESSION['etchat_'.$this->_prefix.'block_all']) && is_array ($_SESSION['etchat_'.$this->_prefix.'block_all']) && in_array($user_id, $_SESSION['etchat_'.$this->_prefix.'block_all'])) {
+	if (isset ($_SESSION['etchat_'.$this->_prefix.'block_all']) && is_array ($_SESSION['etchat_'.$this->_prefix.'block_all']) && in_array($user_id, $_SESSION['etchat_'.$this->_prefix.'block_all'])) {
 			return true;
 		} else {
 			return false;
 		}
-	#	if (isset ($_SESSION['etchat_'.$this->_prefix.'block_priv']) && is_array ($_SESSION['etchat_'.$this->_prefix.'block_priv']) && in_array($user_id, $_SESSION['etchat_'.$this->_prefix.'block_priv']) && $privat_id==$_SESSION['etchat_'.$this->_prefix.'user_id'])  {
+		if (isset ($_SESSION['etchat_'.$this->_prefix.'block_priv']) && is_array ($_SESSION['etchat_'.$this->_prefix.'block_priv']) && in_array($user_id, $_SESSION['etchat_'.$this->_prefix.'block_priv']) && $privat_id==$_SESSION['etchat_'.$this->_prefix.'user_id'])  {
 			return true;
 		} else {
 			return false;
